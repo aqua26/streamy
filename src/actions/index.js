ff85e620ab1811175d60b01ce1d344d51c8bb415
Akshay <aqua26@gmail.com>
2020-08-17 15:05:12 +0530

    Updates with Delete Buttons

--- src/actions/index.js
+++ src/actions/index.js
@@ -50,9 +50,9 @@ export const editStream = (id, formValues) => async dispatch => {
     history.push('/');
 };
 
-export const deleteStream = (id) => async dispatch => {
-    // eslint-disable-next-line
-    await streams.delete('/streams/${id}');
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id});
+    history.push('/');
 };
